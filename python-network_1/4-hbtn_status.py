b21353625f145245f8673142bcf5424c1d8411ec
igiranezah59 <higiranez@gmail.com>
2024-03-25 09:57:04 +0200

    Fix hbtn status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,9 +4,13 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-if __name__ == '__main__':
 
-    res = requests.get("https://intranet.hbtn.io/status")
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+    res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
