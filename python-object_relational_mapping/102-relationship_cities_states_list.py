8022fd9ee0a609179d25d7bfc245d399c0ea287f
Honorine <54628355+honorine22@users.noreply.github.com>
2024-04-05 13:10:47 +0200

    Update 102-relationship_cities_states_list.py

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-ORM queries that modify the databse
-
-"""
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    # lets create a Session object
-    ses = Session()
-    out = ses.query(City).order_by(City.id)
-    for ciudad in out:
-        print("{}: {} -> {}".format(ciudad.id, ciudad.name, ciudad.state.name))
-    ses.close()
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
