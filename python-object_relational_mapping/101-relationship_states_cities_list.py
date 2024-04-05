6f596255bb8203ce47921e2146dfebc491720615
Honorine <54628355+honorine22@users.noreply.github.com>
2024-04-05 13:10:04 +0200

    Update 101-relationship_states_cities_list.py

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-ORM queries that modify the databse
-
-"""
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    # lets create a Session object
-    ses = Session()
-    out = ses.query(State).order_by(State.id)
-    for estado in out:
-        print("{}: {}".format(estado.id, estado.name))
-        for ciudad in estado.cities:
-            print("    {}: {}".format(ciudad.id, ciudad.name))
-    ses.close()
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
